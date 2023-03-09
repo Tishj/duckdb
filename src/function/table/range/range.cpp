5228e3b4cf64b30ef44c74747781384fd5119d85
Tishj <t_b@live.nl>
2023-03-09 10:42:36 +0100

    'functional' in-out range function

--- src/function/table/range/range.cpp
+++ src/function/table/range/range.cpp
@@ -9,18 +9,108 @@
 namespace duckdb {
 
 struct RangeInOutFunctionState : public GlobalTableFunctionState {
-	RangeInOutFunctionState() : current_idx(0) {
+	using range_t = int32_t;
+
+	RangeInOutFunctionState() : new_row(true), input_idx(0), range_idx(0) {
 	}
 
-	hugeint_t start;
-	hugeint_t end;
-	hugeint_t increment;
+	struct RangeSettings {
+		range_t start;
+		range_t end;
+		range_t increment;
+		//! The size of the range for the current input
+		idx_t size;
+	};
 
-	void Update() {
+	enum class SettingType { START, END, INCREMENT };
 
+	RangeSettings settings;
+
+	template <SettingType SETTING>
+	struct SettingContainer {
+	public:
+		SettingContainer() : initialized(false) {
+		}
+		UnifiedVectorFormat format;
+		bool initialized = false;
+
+	public:
+		void Set(idx_t count, Vector &vec) {
+			vec.ToUnifiedFormat(count, format);
+			initialized = true;
+		}
+		range_t Get(idx_t idx) {
+			if (initialized) {
+				idx = format.sel->get_index(idx);
+				return ((range_t *)format.data)[idx];
+			}
+			if (SETTING == SettingType::START) {
+				return 0;
+			}
+			if (SETTING == SettingType::INCREMENT) {
+				return 1;
+			}
+			throw InternalException("'end' of a range should always be given");
+		}
+	};
+
+	SettingContainer<SettingType::START> start_data;
+	SettingContainer<SettingType::END> end_data;
+	SettingContainer<SettingType::INCREMENT> increment_data;
+
+public:
+	RangeSettings &GetCurrentSettings(DataChunk &chunk) {
+		if (input_idx == 0) {
+			// Initialize the vector formats for the entire chunk
+			if (chunk.ColumnCount() == 1) {
+				end_data.Set(chunk.size(), chunk.data[0]);
+			} else if (chunk.ColumnCount() == 2) {
+				start_data.Set(chunk.size(), chunk.data[0]);
+				end_data.Set(chunk.size(), chunk.data[1]);
+			} else if (chunk.ColumnCount() == 3) {
+				start_data.Set(chunk.size(), chunk.data[0]);
+				end_data.Set(chunk.size(), chunk.data[1]);
+				increment_data.Set(chunk.size(), chunk.data[2]);
+			} else {
+				throw InvalidInputException("'range' expects 1, 2 or 3 arguments!");
+			}
+		}
+		if (range_idx == 0) {
+			// New range starts, need to refresh the current settings
+			settings.start = start_data.Get(input_idx);
+			settings.end = end_data.Get(input_idx);
+			settings.increment = increment_data.Get(input_idx);
+
+			int64_t offset = settings.increment < 0 ? 1 : -1;
+			settings.size = Hugeint::Cast<idx_t>((settings.end + (settings.increment + offset)) / settings.increment);
+		}
+		return settings;
 	}
+
+	OperatorResultType Update(idx_t written_tuples, idx_t input_size) {
+		if (written_tuples == 0) {
+			return OperatorResultType::HAVE_MORE_OUTPUT;
+		}
+		if (range_idx + written_tuples == settings.size) {
+			input_idx++;
+			range_idx = 0;
+		}
+		if (input_idx == input_size) {
+			input_idx = 0;
+			return OperatorResultType::NEED_MORE_INPUT;
+		}
+		return OperatorResultType::HAVE_MORE_OUTPUT;
+	}
+
+	idx_t GetRemaining(const RangeSettings &settings, idx_t written_tuples) {
+		D_ASSERT(range_idx < settings.size);
+		D_ASSERT(STANDARD_VECTOR_SIZE > written_tuples);
+		return MinValue<idx_t>(settings.size - range_idx, STANDARD_VECTOR_SIZE - written_tuples);
+	}
+
 	bool new_row;
-	int64_t current_idx;
+	idx_t input_idx;
+	idx_t range_idx;
 };
 
 static unique_ptr<GlobalTableFunctionState> RangeFunctionInit(ClientContext &context, TableFunctionInitInput &input) {
@@ -39,35 +129,49 @@ static unique_ptr<FunctionData> RangeFunctionBind(ClientContext &context, TableF
 	return make_unique<TableFunctionData>();
 }
 
-static const hugeint_t &GetStart(DataChunk &input, idx_t row, idx_t count) {
-	UnifiedVectorFormat data;
-	input.data[0].ToUnifiedFormat(count, data);
-	auto idx = data.sel->get_index(row);
-	return ((hugeint_t*)data.data)[idx];
-}
-
 static OperatorResultType RangeFunction(ExecutionContext &context, TableFunctionInput &data_p, DataChunk &input,
                                         DataChunk &output) {
 	auto &state = (RangeInOutFunctionState &)*data_p.global_state;
 
-	D_ASSERT(input.ColumnCount() >= 1);
-	auto increment = bind_data.increment;
-	auto end = bind_data.end;
-	hugeint_t current_value = bind_data.start + increment * state.current_idx;
-	int64_t current_value_i64;
-	if (!Hugeint::TryCast<int64_t>(current_value, current_value_i64)) {
-		return;
-	}
-	int64_t offset = increment < 0 ? 1 : -1;
-	idx_t remaining = MinValue<idx_t>(Hugeint::Cast<idx_t>((end - current_value + (increment + offset)) / increment),
-									STANDARD_VECTOR_SIZE);
-	// set the result vector as a sequence vector
-	output.data[0].Sequence(current_value_i64, Hugeint::Cast<int64_t>(increment), remaining);
-	// increment the index pointer by the remaining count
-	state.current_idx += remaining;
-	output.SetCardinality(remaining);
+	using range_t = RangeInOutFunctionState::range_t;
+	idx_t total_written_tuples = 0;
+	idx_t written_tuples = 0;
+	OperatorResultType result;
 
-	return OperatorResultType::NEED_MORE_INPUT;
+	// Either we reach the end of the input chunk, or we have written an entire chunk of output
+	while ((result = state.Update(written_tuples, input.size())) != OperatorResultType::NEED_MORE_INPUT &&
+	       total_written_tuples < STANDARD_VECTOR_SIZE) {
+		auto &settings = state.GetCurrentSettings(input);
+		auto &increment = settings.increment;
+		auto &start = settings.start;
+
+		auto remaining = state.GetRemaining(settings, total_written_tuples);
+
+		range_t current_value = start + increment * state.range_idx;
+		int64_t current_value_i64;
+		if (!Hugeint::TryCast<int64_t>(current_value, current_value_i64)) {
+			throw InvalidInputException("Range value exceeds the capacity of BIGINT");
+		}
+		auto increment_i64 = Hugeint::Cast<int64_t>(increment);
+		if (remaining == STANDARD_VECTOR_SIZE) {
+			// We can write a sequence vector to be efficient, the entire output is populated by one range
+			output.data[0].Sequence(current_value_i64, Hugeint::Cast<int64_t>(increment), remaining);
+		} else {
+			// FIXME: might be faster to also return a sequence vector and just return HAVE MORE OUTPUT?
+			UnifiedVectorFormat output_data;
+			output.data[0].ToUnifiedFormat(remaining, output_data);
+			auto result_data = (int64_t *)(output_data.data);
+			for (idx_t i = 0; i < remaining; i++) {
+				auto idx = output_data.sel->get_index(i + total_written_tuples);
+				result_data[idx] = current_value_i64 + (increment_i64 * i);
+			}
+		}
+		written_tuples = remaining;
+		total_written_tuples += written_tuples;
+	}
+
+	output.SetCardinality(total_written_tuples);
+	return result;
 }
 
 void RangeInOutTableFunction::RegisterFunction(TableFunctionSet &set) {
