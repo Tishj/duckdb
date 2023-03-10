c1ee593ddca4c7418fc8fa019e5498b1066ae27d
Tishj <t_b@live.nl>
2023-03-10 16:23:41 +0100

    add missing file

---
+++ src/include/duckdb/function/table/range/range_inout_execution.hpp
@@ -0,0 +1,171 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/function/table/range/range_inout_execution.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "duckdb/function/table_function.hpp"
+#include "duckdb/function/built_in_functions.hpp"
+#include "duckdb/function/table_function.hpp"
+#include "duckdb/function/function_set.hpp"
+#include "duckdb/common/types/vector.hpp"
+
+namespace duckdb {
+
+namespace range {
+
+template <class START_TYPE, class END_TYPE, class INCREMENT_TYPE>
+struct RangeSettings {
+	START_TYPE start;
+	END_TYPE end;
+	INCREMENT_TYPE increment;
+	//! The size of the range for the current input
+	idx_t size;
+};
+
+template <class TYPE>
+struct SettingContainer {
+public:
+	SettingContainer(TYPE default_value) : initialized(false), default_value(std::move(default_value)) {
+	}
+	UnifiedVectorFormat format;
+	bool initialized = false;
+	TYPE default_value;
+
+public:
+	void Set(idx_t count, Vector &vec) {
+		vec.ToUnifiedFormat(count, format);
+		initialized = true;
+	}
+	TYPE Get(idx_t idx) {
+		if (initialized) {
+			idx = format.sel->get_index(idx);
+			return ((TYPE *)format.data)[idx];
+		}
+		return default_value;
+	}
+};
+
+class RangeExecutor {
+public:
+	RangeExecutor() : new_row(false), input_idx(0), range_idx(0) {
+	}
+	virtual ~RangeExecutor() {
+	}
+
+public:
+	virtual OperatorResultType Update(idx_t written_tuples, idx_t input_size) = 0;
+	virtual idx_t Execute(ExecutionContext &context, DataChunk &input, DataChunk &output, idx_t total_written) = 0;
+
+protected:
+	bool new_row;
+	idx_t input_idx;
+	idx_t range_idx;
+};
+
+template <class RANGE_TYPE>
+class RangeIntExecutor : public RangeExecutor {
+	using range_t = RANGE_TYPE;
+	using increment_t = RANGE_TYPE;
+	using IntRange = RangeSettings<range_t, range_t, increment_t>;
+
+public:
+	RangeIntExecutor() : settings(), start_data(0), end_data(0), increment_data(1) {
+	}
+	virtual ~RangeIntExecutor() {
+	}
+
+public:
+	OperatorResultType Update(idx_t written_tuples, idx_t input_size) {
+		if (written_tuples == 0) {
+			return OperatorResultType::HAVE_MORE_OUTPUT;
+		}
+		if (range_idx + written_tuples == settings.size) {
+			input_idx++;
+			range_idx = 0;
+		}
+		if (input_idx == input_size) {
+			input_idx = 0;
+			return OperatorResultType::NEED_MORE_INPUT;
+		}
+		return OperatorResultType::HAVE_MORE_OUTPUT;
+	}
+
+	idx_t Execute(ExecutionContext &context, DataChunk &input, DataChunk &output, idx_t total_written) {
+		auto &settings = GetCurrentSettings(input);
+		auto &increment = settings.increment;
+		auto &start = settings.start;
+
+		auto remaining = GetRemaining(settings, total_written);
+
+		range_t current_value = start + increment * range_idx;
+		int64_t current_value_i64;
+		if (!Hugeint::TryCast<int64_t>(current_value, current_value_i64)) {
+			throw InvalidInputException("Range value exceeds the capacity of BIGINT");
+		}
+		auto increment_i64 = Hugeint::Cast<int64_t>(increment);
+		if (remaining == STANDARD_VECTOR_SIZE) {
+			// We can write a sequence vector to be efficient, the entire output is populated by one range
+			output.data[0].Sequence(current_value_i64, Hugeint::Cast<int64_t>(increment), remaining);
+		} else {
+			// FIXME: might be faster to also return a sequence vector and just return HAVE MORE OUTPUT?
+			UnifiedVectorFormat output_data;
+			output.data[0].ToUnifiedFormat(remaining, output_data);
+			auto result_data = (int64_t *)(output_data.data);
+			for (idx_t i = 0; i < remaining; i++) {
+				auto idx = output_data.sel->get_index(i + total_written);
+				result_data[idx] = current_value_i64 + (increment_i64 * i);
+			}
+		}
+		return remaining;
+	}
+
+private:
+	IntRange &GetCurrentSettings(DataChunk &chunk) {
+		if (input_idx == 0) {
+			// Initialize the vector formats for the entire chunk
+			if (chunk.ColumnCount() == 1) {
+				end_data.Set(chunk.size(), chunk.data[0]);
+			} else if (chunk.ColumnCount() == 2) {
+				start_data.Set(chunk.size(), chunk.data[0]);
+				end_data.Set(chunk.size(), chunk.data[1]);
+			} else if (chunk.ColumnCount() == 3) {
+				start_data.Set(chunk.size(), chunk.data[0]);
+				end_data.Set(chunk.size(), chunk.data[1]);
+				increment_data.Set(chunk.size(), chunk.data[2]);
+			} else {
+				throw InvalidInputException("'range' expects 1, 2 or 3 arguments!");
+			}
+		}
+		if (range_idx == 0) {
+			// New range starts, need to refresh the current settings
+			settings.start = start_data.Get(input_idx);
+			settings.end = end_data.Get(input_idx);
+			settings.increment = increment_data.Get(input_idx);
+
+			int64_t offset = settings.increment < 0 ? 1 : -1;
+			settings.size = Hugeint::Cast<idx_t>((settings.end + (settings.increment + offset)) / settings.increment);
+		}
+		return settings;
+	}
+
+	idx_t GetRemaining(const IntRange &settings, idx_t written_tuples) {
+		D_ASSERT(range_idx < settings.size);
+		D_ASSERT(STANDARD_VECTOR_SIZE > written_tuples);
+		return MinValue<idx_t>(settings.size - range_idx, STANDARD_VECTOR_SIZE - written_tuples);
+	}
+
+private:
+	IntRange settings;
+	SettingContainer<range_t> start_data;
+	SettingContainer<range_t> end_data;
+	SettingContainer<increment_t> increment_data;
+};
+
+} // namespace range
+
+} // namespace duckdb
