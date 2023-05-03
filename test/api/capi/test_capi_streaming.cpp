fa58dc5d2194f827c7be62ef49ec61acb23bf93a
Tishj <t_b@live.nl>
2023-05-03 16:34:56 +0200

    suspected segfault but works fine..

--- test/api/capi/test_capi_streaming.cpp
+++ test/api/capi/test_capi_streaming.cpp
@@ -7,28 +7,28 @@ using namespace std;
 TEST_CASE("Test streaming results in C API", "[capi]") {
 	CAPITester tester;
 	CAPIPrepared prepared;
-	CAPIPending pending;
 
 	// open the database in in-memory mode
 	REQUIRE(tester.OpenDatabase(nullptr));
 	REQUIRE(prepared.Prepare(tester, "SELECT * from read_parquet('/Users/thijs/DuckDBLabs/duckdb/tmp/HelloDuckDB/resources/types_nested.parquet')"));
-	REQUIRE(pending.PendingStreaming(prepared));
 
-	while (true) {
-		auto state = pending.ExecuteTask();
-		REQUIRE(state != DUCKDB_PENDING_ERROR);
-		if (state == DUCKDB_PENDING_RESULT_READY) {
-			break;
-		}
-	}
 
+	duckdb::vector<duckdb::unique_ptr<CAPIDataChunk>> chunks;
 	for (idx_t i = 0; i < 10; i++) {
+		CAPIPending pending;
+		REQUIRE(pending.PendingStreaming(prepared));
+		while (true) {
+			auto state = pending.ExecuteTask();
+			REQUIRE(state != DUCKDB_PENDING_ERROR);
+			if (state == DUCKDB_PENDING_RESULT_READY) {
+				break;
+			}
+		}
 		auto result = pending.Execute();
 		REQUIRE(result);
 		REQUIRE(!result->HasError());
 
 		idx_t chunk_count = 0;
-		duckdb::vector<duckdb::unique_ptr<CAPIDataChunk>> chunks;
 		while (true) {
 			auto chunk = result->StreamChunk();
 			if (chunk == nullptr) {
@@ -36,14 +36,11 @@ TEST_CASE("Test streaming results in C API", "[capi]") {
 			}
 			auto vector = chunk->GetVector(0);
 			chunk_count++;
-			if (chunk_count % 2 == 0) {
-				continue;
-			}
-			chunks.emplace_back(std::move(chunk));
+			chunks.push_back(std::move(chunk));
 		}
-		for (auto i = chunks.rbegin(); i != chunks.rend(); i++) {
-			auto ptr = std::move(*i);
-		}
+	}
+	for (auto i = chunks.rbegin(); i != chunks.rend(); i++) {
+		auto ptr = std::move(*i);
 	}
 }
 
