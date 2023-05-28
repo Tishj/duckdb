204e52e65877b718a3b126a15ee972410a13c0ae
Tishj <t_b@live.nl>
2023-05-28 13:28:39 +0200

    add some debugging prints

--- src/function/table/arrow.cpp
+++ src/function/table/arrow.cpp
@@ -21,6 +21,15 @@ static ArrowConvertData &GetConvertData(unordered_map<idx_t, unique_ptr<ArrowCon
 		D_ASSERT(insert.second);
 	}
 	D_ASSERT(arrow_convert_data.count(col_idx));
+	if (!arrow_convert_data[col_idx]) {
+		if (!arrow_convert_data.empty()) {
+			for (auto &entry : arrow_convert_data) {
+				Printer::Print(StringUtil::Format("[%d] : %p", entry.first, (void *)entry.second.get()));
+			}
+		} else {
+			Printer::Print("ARROW_CONVERT_DATA IS EMPTY");
+		}
+	}
 	return *arrow_convert_data[col_idx];
 }
 
