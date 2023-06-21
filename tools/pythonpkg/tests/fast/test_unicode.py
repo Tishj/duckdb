8f792e5e1cdc0db49e123dab1962321627b5cff6
Tishj <t_b@live.nl>
2023-06-21 14:59:07 +0200

    add unicode test:

--- tools/pythonpkg/tests/fast/test_unicode.py
+++ tools/pythonpkg/tests/fast/test_unicode.py
@@ -14,3 +14,21 @@ class TestUnicode(object):
     def test_unicode_types(self):
         con = duckdb.connect()
         
+        test_df = pd.DataFrame.from_dict({
+            'ascii': chr(56),
+            'ascii_min': chr(0),
+            'ascii_max': chr(127),
+            '1byte_utf8': chr(156),
+            '1byte_utf8_min': chr(128),
+            '1byte_utf8_max': chr(255),
+            '2byte_utf8': chr(425),
+            '2byte_utf8_min': chr(256),
+            '2byte_utf8_max': chr(2047),
+            '3byte_utf8': chr(34532),
+            '3byte_utf8_min': chr(2048),
+            '3byte_utf8_max': chr(65535),
+            '4byte_utf8': chr(234234),
+            '4byte_utf8_min': chr(65536),
+            '4byte_utf8_max': chr(1114111)
+        })
+        print(test_df)
