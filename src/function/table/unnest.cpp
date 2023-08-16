e6ceec75a47eeb2661367c2909d572efa6bcef69
Tishj <t_b@live.nl>
2023-08-16 14:41:25 +0200

    put back some accidentally removed changes

--- src/function/table/unnest.cpp
+++ src/function/table/unnest.cpp
@@ -78,7 +78,8 @@ static OperatorResultType UnnestFunction(ExecutionContext &context, TableFunctio
 }
 
 void UnnestTableFunction::RegisterFunction(BuiltinFunctions &set) {
-	TableFunction unnest_function("unnest", {LogicalTypeId::TABLE}, nullptr, UnnestBind, UnnestInit, UnnestLocalInit);
+	TableFunction unnest_function("unnest", {}, nullptr, UnnestBind, UnnestInit, UnnestLocalInit);
+	unnest_function.varargs = LogicalType::ANY;
 	unnest_function.in_out_function = UnnestFunction;
 	set.AddFunction(unnest_function);
 }
