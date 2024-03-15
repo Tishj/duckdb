84a302f13a879c0d5168df5c4c2f6bb3c373a5db
Tishj <t_b@live.nl>
2024-03-15 17:30:56 +0100

    ...

--- scripts/arrow_copy_costs.py
+++ scripts/arrow_copy_costs.py
@@ -36,7 +36,7 @@ def close_result():
 
 ### --- Zero Copy
 
-types = [
+types = {
     'NULL',
     'TINYINT',
     'UTINYINT',
@@ -55,10 +55,8 @@ types = [
     'TIMESTAMP_S',
     'TIMESTAMP_MS',
     'DATE',  # <-- date32 [days] in Arrow
-    'DECIMAL(38, 36)',
-]
-
-types = ['HUGEINT']
+    'DECIMAL(38, 10)',
+}
 
 # Arrays of this type are kept alive and their data buffers are referenced in the DuckDB Vectors
 # If no destructive transformation is applied to them
@@ -80,13 +78,24 @@ con.query("select current_setting('arrow_disable_zero_copy')").show()
 # Warm up the analyzer
 con.query("explain analyze select 42")
 
+def get_query(type: str):
+    if type == 'NULL':
+        return "select [NULL, NULL, NULL] as lst"
+    type_mapping = {
+        'DECIMAL(38, 10)': 'dec38_10',
+        'DECIMAL(18, 6)': 'dec_18_6',
+        'DECIMAL(9, 4)': 'dec_9_4',
+        'DECIMAL(4, 1)': 'dec_4_1',
+    }
+    if type in type_mapping:
+        type = type_mapping[type]
+    return f"""select list("{type}") as lst from test_all_types()"""
+
 for type in types:
     for batch_size in BATCH_SIZES:
         for size in TUPLE_COUNTS:
             for nrun in range(10):
-                list_query = f"""
-                    select list("{type}") as lst from test_all_types()
-                """
+                list_query = get_query(type)
 
                 # Create the DuckDB relation we will use to create the arrow table
                 rel = con.query(
@@ -112,7 +121,7 @@ for type in types:
                 res = con.query("EXPLAIN ANALYZE select * from arrow_table").fetchall()
                 end = time.time()
                 duration = float(end - start)
-                write_result(f"arrow_table_tuple_count_{size}_batch_size_{batch_size}", nrun, duration)
+                write_result(f"arrow_table_{type}_tuple_count_{size}_batch_size_{batch_size}", nrun, duration)
 
 ### --- Partial Copy
 
@@ -132,9 +141,9 @@ types = [
     'TIME',
     'TIMESTAMPTZ',
     'INTERVAL',
-    'DECIMAL(4, 2)',  # <-- DECIMAL (SMALLINT)
-    'DECIMAL(9, 6)',  # <-- DECIMAL (INTEGER)
-    'DECIMAL(18, 12)',  # <-- DECIMAL (INTEGER)
+    'DECIMAL(4, 1)',  # <-- DECIMAL (SMALLINT)
+    'DECIMAL(9, 4)',  # <-- DECIMAL (INTEGER)
+    'DECIMAL(18, 6)',  # <-- DECIMAL (INTEGER)
 ]
 
 # For these types we always copy
